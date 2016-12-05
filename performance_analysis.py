3d846296662858a4056720262c4eb2ef17cfc4ac
Julia <juliageist23@gmail.com>
2016-12-04 17:16:27 -0800

    file to call each structure and to get the avg

---
+++ performance_analysis.py
@@ -0,0 +1,6 @@
+import dictionary, hashtable, singly_linked_list, sorted_list_of_tuples, sorted_singly_linked_list, trie, binary_search_tree
+import time
+
+if __name__ == "__main__":
+	for i in range(100):
+		# call functions and calculate the average time 
