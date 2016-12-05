37d3f9466b816df926424168db155390477c47f3
Julia <juliageist23@gmail.com>
2016-12-04 17:15:50 -0800

    implementation and setup for benchmarking

---
+++ sorted_list_of_tuples.py
@@ -0,0 +1,36 @@
+import sys
+import time
+start_time = time.time()
+
+def generateHistogramFromFile(textfile):
+	# open text file 
+	f = open(textfile, 'r')
+
+	# read the file
+	fileContent = f.read()
+
+	# fileContent = fileContent.encode('utf-8', 'ignore')
+
+	# generate array from file content
+	wordsArray = fileContent.strip().replace('\n', ' ').split(' ')
+
+	# generate histogram from words array
+	histogram = {}
+	for word in wordsArray:
+		if word in histogram:
+			histogram[word] = histogram[word] + 1
+		else:
+			histogram[word] = 1
+
+	# function called zip --> tuple
+	# hash function used to implement dictionaries
+	sortedListOfTuples = sorted(histogram.items(), key=operator.itemgetter(1))
+	print("--- %s seconds ---" % (time.time() - start_time))
+	return sortedListOfTuples
+
+def count(word, histogram):
+	print("--- %s seconds ---" % (time.time() - start_time))
+	return [v for i, v in histogram if i == word]
+
+if __name__ == "__main__":
+	generateHistogramFromFile(sys.argv[1])
